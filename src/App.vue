1805362a85480b5dfcf67806aa973f5e0c1d16aa
Muhammad Daivasmara Denaw <daivasmara@gmail.com>
2018-10-01 14:32:27 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="container">
+      <div class="row">
+        <h1 class="col s6 offset-s3 center-align teal-text">To-Do List!</h1>
+      </div>
+      <div class="row">
+        <form @submit.prevent="submitTodo" class="col s6 offset-s3">
+          <div class="input-field">
+            <i class="material-icons prefix">list</i>
+            <textarea v-model="newTodo" id="icon_prefix2" class="materialize-textarea"></textarea>
+            <label for="icon_prefix2">What to do?</label>
+          </div>
+          <button class="btn waves-effect col s12">Add</button>
+        </form>
+      </div>
+      <div class="row">
+        <ul class="collection col s6 offset-s3">
+          <li class="collection-item" v-for="todo in todos">
+            <p>
+              <label>
+                <input type="checkbox" :checked=todo.done @change="todo.done = !todo.done" />
+                <span>{{todo.title}}</span>
+                <span><a @click.prevent="deleteTodo(todo)"><i class="material-icons right teal-text">delete</i></a></span>
+              </label>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todos: [],
+      newTodo: ''
+    }
   },
+  methods: {
+    submitTodo() {
+      this.todos.push({
+        title: this.newTodo,
+        done: false
+      })
+      this.newTodo = ''
+    },
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1);
+    }
+  }
+
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import '../node_modules/materialize-css/sass/materialize.scss';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
 Vue.config.productionTip = false;
 
